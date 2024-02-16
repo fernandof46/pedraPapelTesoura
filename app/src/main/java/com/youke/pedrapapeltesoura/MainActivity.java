04bc6f59951593c22c3b43572596f9b2866ccfaf
Fernando Ferreira <121515671+fernandof46@users.noreply.github.com>
2024-02-16 11:36:45 -0300

    Initial commit

---
+++ app/src/main/java/com/youke/pedrapapeltesoura/MainActivity.java
@@ -0,0 +1,80 @@
+package com.youke.pedrapapeltesoura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*
+    criando os métodos de escolha do usuário, devem ser público para serem vistos pelo app
+    o método verificar ganhador informa ao app a escolha do usuário e compara com a escolha
+    aleatória do aplicativo
+    */
+    public void selecionarPedra(View view) {
+        verificarGanhador("pedra");
+    }
+
+    public void selecionarPapel(View view) {
+        verificarGanhador("papel");
+    }
+
+    public void selecionarTesoura(View view) {
+        verificarGanhador("tesoura");
+    }
+
+    private String gerarEscolhaAleatoriaApp() {
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        int numeroAleatorio = new Random().nextInt(3);
+
+        ImageView imagemApp = findViewById(R.id.imageAPP);
+        String escolhaApp = opcoes[numeroAleatorio];
+        switch (escolhaApp) {
+            case "pedra":
+                imagemApp.setImageResource(R.drawable.pedra);
+                break;
+            case "paepl":
+                imagemApp.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imagemApp.setImageResource(R.drawable.tesoura);
+                break;
+        }
+        return escolhaApp;
+    }
+
+    private void verificarGanhador(String escolhaUsuario) {
+        String escolhaApp = gerarEscolhaAleatoriaApp();
+        System.out.println("Item clicado:" + escolhaUsuario);
+
+        TextView textoResultado = findViewById(R.id.text_resultado);
+        if ((escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+            (escolhaApp == "papel" && escolhaUsuario == "pedra") ||
+            (escolhaApp == "tesoura" && escolhaUsuario == "papel")
+        ) {
+            textoResultado.setText("voce perdeu");
+        } else if (//usuario vencedor
+
+                (escolhaApp == "tesoura" && escolhaUsuario == "pedra") ||
+                (escolhaApp == "pedra" && escolhaUsuario == "papel") ||
+                (escolhaApp == "papel" && escolhaUsuario == "tesoura")
+        ) {
+            textoResultado.setText("voce ganhou");
+        } else { //empate
+            textoResultado.setText("Empate");
+        }
+    }
+}
+
+
